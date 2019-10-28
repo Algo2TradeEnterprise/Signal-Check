7f08d6a190ad45fc88a7725f6b3dcb1d4229238f
RIO <RIO@DESKTOP-RIO>
2019-10-28 16:12:05 +0530

    From Sertver

--- Algo2Trade Signal Check/StrategyRules/DayHighLow.vb
+++ Algo2Trade Signal Check/StrategyRules/DayHighLow.vb
@@ -15,6 +15,7 @@ Public Class DayHighLow
         ret.Columns.Add("High")
         ret.Columns.Add("Close")
         ret.Columns.Add("Volume")
+        ret.Columns.Add("Signal Candle")
         ret.Columns.Add("Candle Change")
         ret.Columns.Add("Volume Change")
         ret.Columns.Add("Direction")
@@ -83,66 +84,102 @@ Public Class DayHighLow
                             Dim firstCandle As Boolean = True
                             Dim highSignalCandle As Payload = Nothing
                             Dim lowSignalCandle As Payload = Nothing
+                            Dim highestCandleOfTheDay As Payload = Nothing
+                            Dim lowestCandleOfTheDay As Payload = Nothing
                             For Each runningPayload In currentDayPayload.Keys
                                 _canceller.Token.ThrowIfCancellationRequested()
                                 If Not firstCandle Then
                                     If highSignalCandle Is Nothing Then
-                                        Dim dayHigh As Decimal = currentDayPayload.Max(Function(x)
-                                                                                           If x.Key < runningPayload Then
-                                                                                               Return x.Value.High
-                                                                                           Else
-                                                                                               Return Decimal.MinValue
-                                                                                           End If
-                                                                                       End Function)
-
-                                        If currentDayPayload(runningPayload).PreviousCandlePayload.High = dayHigh Then
-                                            highSignalCandle = currentDayPayload(runningPayload)
+                                        If currentDayPayload(runningPayload).PreviousCandlePayload.PayloadDate = highestCandleOfTheDay.PayloadDate Then
+                                            If currentDayPayload(runningPayload).PreviousCandlePayload.CandleColor = Color.Green Then
+                                                highSignalCandle = currentDayPayload(runningPayload).PreviousCandlePayload
+                                            End If
+                                        Else
+                                            If highestCandleOfTheDay.CandleRange < ATRPayload(highestCandleOfTheDay.PayloadDate) * 2 Then
+                                                If currentDayPayload(runningPayload).PreviousCandlePayload.High > highestCandleOfTheDay.Low Then
+                                                    If currentDayPayload(runningPayload).PreviousCandlePayload.CandleColor = Color.Green Then
+                                                        highSignalCandle = currentDayPayload(runningPayload).PreviousCandlePayload
+                                                    End If
+                                                End If
+                                            End If
                                         End If
                                     Else
-                                        If highSignalCandle.CandleRange >= ATRPayload(highSignalCandle.PayloadDate) * 2 Then
-                                            If currentDayPayload(runningPayload).PreviousCandlePayload.High > highSignalCandle.High Then
-                                                highSignalCandle = currentDayPayload(runningPayload)
+                                        If currentDayPayload(runningPayload).PreviousCandlePayload.High > highSignalCandle.High Then
+                                            If currentDayPayload(runningPayload).PreviousCandlePayload.CandleColor = Color.Green Then
+                                                highSignalCandle = currentDayPayload(runningPayload).PreviousCandlePayload
+                                            End If
+                                        Else
+                                            If highSignalCandle.CandleRange < ATRPayload(highSignalCandle.PayloadDate) * 2 Then
+                                                If Not IsInsideBar(highSignalCandle, currentDayPayload(runningPayload).PreviousCandlePayload) Then
+                                                    If currentDayPayload(runningPayload).PreviousCandlePayload.High > currentDayPayload(runningPayload).PreviousCandlePayload.PreviousCandlePayload.High Then
+                                                        If currentDayPayload(runningPayload).PreviousCandlePayload.High > highSignalCandle.Low Then
+                                                            If currentDayPayload(runningPayload).PreviousCandlePayload.CandleColor = Color.Green Then
+                                                                highSignalCandle = currentDayPayload(runningPayload).PreviousCandlePayload
+                                                            End If
+                                                        End If
+                                                    End If
+                                                End If
                                             End If
                                         End If
                                     End If
 
                                     If lowSignalCandle Is Nothing Then
-                                        Dim dayLow As Decimal = currentDayPayload.Min(Function(x)
-                                                                                          If x.Key < runningPayload Then
-                                                                                              Return x.Value.Low
-                                                                                          Else
-                                                                                              Return Decimal.MaxValue
-                                                                                          End If
-                                                                                      End Function)
-
-                                        If currentDayPayload(runningPayload).PreviousCandlePayload.Low = dayLow Then
-                                            lowSignalCandle = currentDayPayload(runningPayload)
+                                        If currentDayPayload(runningPayload).PreviousCandlePayload.PayloadDate = lowestCandleOfTheDay.PayloadDate Then
+                                            If currentDayPayload(runningPayload).PreviousCandlePayload.CandleColor = Color.Red Then
+                                                lowSignalCandle = currentDayPayload(runningPayload).PreviousCandlePayload
+                                            End If
+                                        Else
+                                            If lowestCandleOfTheDay.CandleRange < ATRPayload(lowestCandleOfTheDay.PayloadDate) * 2 Then
+                                                If currentDayPayload(runningPayload).PreviousCandlePayload.Low < lowestCandleOfTheDay.High Then
+                                                    If currentDayPayload(runningPayload).PreviousCandlePayload.CandleColor = Color.Red Then
+                                                        lowSignalCandle = currentDayPayload(runningPayload).PreviousCandlePayload
+                                                    End If
+                                                End If
+                                            End If
                                         End If
                                     Else
-                                        If lowSignalCandle.CandleRange >= ATRPayload(lowSignalCandle.PayloadDate) * 2 Then
-                                            If currentDayPayload(runningPayload).PreviousCandlePayload.Low < lowSignalCandle.Low Then
-                                                lowSignalCandle = currentDayPayload(runningPayload)
+                                        If currentDayPayload(runningPayload).PreviousCandlePayload.Low < lowSignalCandle.Low Then
+                                            If currentDayPayload(runningPayload).PreviousCandlePayload.CandleColor = Color.Red Then
+                                                lowSignalCandle = currentDayPayload(runningPayload).PreviousCandlePayload
+                                            End If
+                                        Else
+                                            If lowSignalCandle.CandleRange < ATRPayload(lowSignalCandle.PayloadDate) * 2 Then
+                                                If Not IsInsideBar(lowSignalCandle, currentDayPayload(runningPayload).PreviousCandlePayload) Then
+                                                    If currentDayPayload(runningPayload).PreviousCandlePayload.Low < currentDayPayload(runningPayload).PreviousCandlePayload.PreviousCandlePayload.Low Then
+                                                        If currentDayPayload(runningPayload).PreviousCandlePayload.Low < lowSignalCandle.High Then
+                                                            If currentDayPayload(runningPayload).PreviousCandlePayload.CandleColor = Color.Red Then
+                                                                lowSignalCandle = currentDayPayload(runningPayload).PreviousCandlePayload
+                                                            End If
+                                                        End If
+                                                    End If
+                                                End If
                                             End If
                                         End If
                                     End If
 
                                     If highSignalCandle IsNot Nothing AndAlso
                                         highSignalCandle.CandleRange < ATRPayload(highSignalCandle.PayloadDate) * 2 Then
-                                        If currentDayPayload(runningPayload).CandleRange >= highSignalCandle.CandleRange AndAlso
-                                            currentDayPayload(runningPayload).Volume < highSignalCandle.Volume Then
+                                        If currentDayPayload(runningPayload).CandleColor = Color.Red AndAlso
+                                            IsStrongClose(currentDayPayload(runningPayload)) AndAlso
+                                            ((currentDayPayload(runningPayload).CandleRange >= highSignalCandle.CandleRange AndAlso
+                                            currentDayPayload(runningPayload).Volume < highSignalCandle.Volume) OrElse
+                                            (currentDayPayload(runningPayload).CandleRange < highSignalCandle.CandleRange AndAlso
+                                            currentDayPayload(runningPayload).Volume <= highSignalCandle.Volume / 2)) Then
                                             Dim row As DataRow = ret.NewRow
-                                            row("Date") = inputPayload(runningPayload).PayloadDate
+                                            row("Date") = inputPayload(runningPayload).PayloadDate.ToString("dd-MMM-yyyy HH:mm:ss")
                                             row("Trading Symbol") = inputPayload(runningPayload).TradingSymbol
                                             row("Open") = inputPayload(runningPayload).Open
                                             row("Low") = inputPayload(runningPayload).Low
                                             row("High") = inputPayload(runningPayload).High
                                             row("Close") = inputPayload(runningPayload).Close
                                             row("Volume") = inputPayload(runningPayload).Volume
+                                            row("Signal Candle") = highSignalCandle.PayloadDate.ToString("HH:mm:ss")
                                             row("Candle Change") = (inputPayload(runningPayload).CandleRange / highSignalCandle.CandleRange) - 1
                                             row("Volume Change") = (inputPayload(runningPayload).Volume / highSignalCandle.Volume) - 1
                                             row("Direction") = -1
 
                                             ret.Rows.Add(row)
+                                            highSignalCandle = Nothing
                                         Else
                                             If Not IsInsideBar(highSignalCandle, currentDayPayload(runningPayload)) Then
                                                 highSignalCandle = Nothing
@@ -152,21 +189,27 @@ Public Class DayHighLow
 
                                     If lowSignalCandle IsNot Nothing AndAlso
                                         lowSignalCandle.CandleRange < ATRPayload(lowSignalCandle.PayloadDate) * 2 Then
-                                        If currentDayPayload(runningPayload).CandleRange >= lowSignalCandle.CandleRange AndAlso
-                                            currentDayPayload(runningPayload).Volume < lowSignalCandle.Volume Then
+                                        If currentDayPayload(runningPayload).CandleColor = Color.Green AndAlso
+                                            IsStrongClose(currentDayPayload(runningPayload)) AndAlso
+                                            ((currentDayPayload(runningPayload).CandleRange >= lowSignalCandle.CandleRange AndAlso
+                                            currentDayPayload(runningPayload).Volume < lowSignalCandle.Volume) OrElse
+                                            (currentDayPayload(runningPayload).CandleRange < lowSignalCandle.CandleRange AndAlso
+                                            currentDayPayload(runningPayload).Volume <= lowSignalCandle.Volume / 2)) Then
                                             Dim row As DataRow = ret.NewRow
-                                            row("Date") = inputPayload(runningPayload).PayloadDate
+                                            row("Date") = inputPayload(runningPayload).PayloadDate.ToString("dd-MMM-yyyy HH:mm:ss")
                                             row("Trading Symbol") = inputPayload(runningPayload).TradingSymbol
                                             row("Open") = inputPayload(runningPayload).Open
                                             row("Low") = inputPayload(runningPayload).Low
                                             row("High") = inputPayload(runningPayload).High
                                             row("Close") = inputPayload(runningPayload).Close
                                             row("Volume") = inputPayload(runningPayload).Volume
+                                            row("Signal Candle") = lowSignalCandle.PayloadDate.ToString("HH:mm:ss")
                                             row("Candle Change") = (inputPayload(runningPayload).CandleRange / lowSignalCandle.CandleRange) - 1
                                             row("Volume Change") = (inputPayload(runningPayload).Volume / lowSignalCandle.Volume) - 1
                                             row("Direction") = 1
 
                                             ret.Rows.Add(row)
+                                            lowSignalCandle = Nothing
                                         Else
                                             If Not IsInsideBar(lowSignalCandle, currentDayPayload(runningPayload)) Then
                                                 lowSignalCandle = Nothing
@@ -174,7 +217,20 @@ Public Class DayHighLow
                                         End If
                                     End If
                                 End If
-
+                                If highestCandleOfTheDay IsNot Nothing Then
+                                    If currentDayPayload(runningPayload).High >= highestCandleOfTheDay.High Then
+                                        highestCandleOfTheDay = currentDayPayload(runningPayload)
+                                    End If
+                                Else
+                                    highestCandleOfTheDay = currentDayPayload(runningPayload)
+                                End If
+                                If lowestCandleOfTheDay IsNot Nothing Then
+                                    If currentDayPayload(runningPayload).Low <= lowestCandleOfTheDay.Low Then
+                                        lowestCandleOfTheDay = currentDayPayload(runningPayload)
+                                    End If
+                                Else
+                                    lowestCandleOfTheDay = currentDayPayload(runningPayload)
+                                End If
                                 firstCandle = False
                             Next
                         End If
@@ -195,6 +251,22 @@ Public Class DayHighLow
         Else
             If currentCandle.Open <= signalCandle.High AndAlso currentCandle.Close >= signalCandle.Low Then
                 ret = True
+            End If
+        End If
+        Return ret
+    End Function
+
+    Private Function IsStrongClose(ByVal candle As Payload) As Boolean
+        Dim ret As Boolean = False
+        If candle IsNot Nothing Then
+            If candle.CandleColor = Color.Green Then
+                If candle.Close >= candle.Low + candle.CandleRange * 75 / 100 Then
+                    ret = True
+                End If
+            ElseIf candle.CandleColor = Color.Red Then
+                If candle.Close <= candle.High - candle.CandleRange * 75 / 100 Then
+                    ret = True
+                End If
             End If
         End If
         Return ret
