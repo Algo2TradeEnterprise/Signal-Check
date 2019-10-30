f457ef0d8fedeec478a1fe51a5899d47c5457b62
Indibar <indibar@algo2trade.com>
2019-10-30 19:44:12 +0530

    After adding new rule

--- Algo2Trade Signal Check/frmMain.vb
+++ Algo2Trade Signal Check/frmMain.vb
@@ -309,6 +309,8 @@ Public Class frmMain
                 rule = New DayHighLow(_canceller, category, name, timeFrame, useHA)
             Case 22
                 rule = New LowSLCandle(_canceller, category, name, timeFrame, useHA)
+            Case 23
+                rule = New InsideBarHighLow(_canceller, category, name, timeFrame, useHA)
         End Select
         AddHandler Rule.Heartbeat, AddressOf OnHeartbeat
         AddHandler Rule.WaitingFor, AddressOf OnWaitingFor
